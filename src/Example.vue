7574a99b9af9c5f9ea1179a3c8e423631623ffed
Kien La <la.kien@gmail.com>
2023-06-04 16:42:53 -0400

    Basic component

--- src/Example.vue
+++ src/Example.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <Number :value="3.14" />
+  <div class="example">
+    <ul>
+      <li><Number :value="3.14" /></li>
+      <li><Number :value="-3.14" /></li>
+    </ul>
   </div>
 </template>
 
 <script setup>
 import Number from './components/Number.vue'
 </script>
+
+<style lang="scss" scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/components/Number.vue
+++ src/components/Number.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
-    {{ value }}
-  </div>
+  <span :class="classes">{{ formattedValue }}</span>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import numbro from 'numbro'
+
 const props = defineProps({
   value: { type: [ Number, String ], required: true },
+  decimals: { type: Number, default: 2 },
+  abbreviated: { type: Boolean, default: false },
+  colored: { type: Boolean, default: true },
+})
+
+const classes = computed(() => {
+  const classes = [ 'vue-number' ]
+  if (props.value < 0) classes.push('negative')
+  if (props.colored) classes.push('colored')
+  return classes.join(' ')
+})
+const formattedValue = computed(() => {
+  return numbro(props.value).formatCurrency({ thousandSeparated: true, mantissa: props.decimals, average: props.abbreviated }).toUpperCase()
 })
 </script>
 
 <style lang="scss" scoped>
+.colored {
+  &.vue-number {
+    color: limegreen;
+  }
+  &.negative {
+    color: indianred;
+  }
+}
 </style>
