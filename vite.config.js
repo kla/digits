e8958c50236be50a1d844b6bab274c35984e06e7
Kien La <la.kien@gmail.com>
2023-06-04 16:24:21 -0400

    add

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    'process.env': {}
+  },
+  esbuild: {
+    minify: true,
+  },
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'VueNumber',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled into your library
+      external: ['vue'],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'style.css')
+            return 'vue-number.css';
+          return assetInfo.name;
+        }
+      }
+    },
+  }
+})
