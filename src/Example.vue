e3470d229b37d94c88dd27fb8157df436673b815
Kien La <la.kien@gmail.com>
2023-06-04 16:36:47 -0400

    setup example

---
+++ src/Example.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Number :value="3.14" />
+  </div>
+</template>
+
+<script setup>
+import Number from './components/Number.vue'
+</script>
---
+++ src/components/Number.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ value }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  value: { type: [ Number, String ], required: true },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
